a3c92c4e7f792f4188f3b2d306767dc5ff67fc92
Sanil Kumar Ashwin <sanil.kumar.ashwin@gmail.com>
2021-07-19 23:23:28 +0100

    Migrated database to postgres

--- caseapi/models.py
+++ caseapi/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+import datetime as dt
 
-# Create your models here.
+class CitedDocs(models.Model):
+    doc_id = models.CharField(max_length=50)  
+    doc_title = models.CharField(max_length=100, null=True)  
+    class Meta:
+        db_table = 'cited_docs'
+        
+class Citation(models.Model):
+    cite_name = models.CharField(max_length=50)
+    doc_id = models.ForeignKey(CitedDocs, on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'citations'
+
+
+class Judges(models.Model):
+    judge_name = models.CharField(max_length=100)
+    judge_id = models.CharField(max_length=100)
+    
+    class Meta:
+        db_table = 'judges'
+        
+class Tags(models.Model):
+    tag = models.CharField(max_length=100)
+    
+    class Meta:
+        db_table = 'tags'
+    
+class FieldTags(models.Model):
+    field_tag = models.CharField(max_length=100)    
+    
+    class Meta:
+        db_table = 'field_tags'
+        
+class CaseMeta(models.Model):
+    
+    title = models.CharField(max_length=255, null=True)
+    document_title = models.CharField(max_length=255, null=True)
+    case_name = models.CharField(max_length=255, null=True)
+    doc_id = models.CharField(max_length=25)
+    doc_type = models.CharField(max_length=50, null=True)
+    judges = models.ManyToManyField(Judges)
+    case_id = models.CharField(max_length=25, null=True)
+    outcome =  models.CharField(max_length=255, null=True)
+    docket_number = models.CharField(max_length = 255, null=True)
+    self_cite = models.CharField(max_length=75, null=True)
+
+    class Meta:
+        db_table = "case_meta"
--- caserepo/settings.py
+++ caserepo/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+
+# Load in environmental variables from the .env file, which must be in same directory as settings.py
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +82,12 @@ WSGI_APPLICATION = 'caserepo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
