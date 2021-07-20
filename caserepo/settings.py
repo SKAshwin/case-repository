fdf2b55d1366f6028b0e16398cd77d52eb44f3b2
Sanil Kumar Ashwin <sanil.kumar.ashwin@gmail.com>
2021-07-20 05:12:27 +0100

    Established basic token authentication

--- caserepo/settings.py
+++ caserepo/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'caseapi'
 ]
 
@@ -116,6 +117,12 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 # Internationalization
