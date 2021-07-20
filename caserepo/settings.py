af8065ce9c6a607abb8f9b1558a9dacbf8f3ccad
Sanil Kumar Ashwin <sanil.kumar.ashwin@gmail.com>
2021-07-20 20:07:58 +0100

    Changed settings to allow for switch to production deployment

--- caserepo/settings.py
+++ caserepo/settings.py
@@ -26,10 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l76t9@d&1hdvf)8-6@xe_z80ho&*!5f5j8)t3e6ckq8ra^^zf@'
+SECRET_KEY = env('DJANGO_SECRETKEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG') # will be false if 'DEBUG' if not set
 
 ALLOWED_HOSTS = [env('PRODUCTION_SERVER'), 'localhost']
 
