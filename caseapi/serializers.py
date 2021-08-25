f0404a73fbd0c307c0106a31371714761162233c
Sanil Kumar Ashwin <sanil.kumar.ashwin@gmail.com>
2021-08-25 23:06:35 +0200

    Labels now used in API instead of internal representation

--- caseapi/serializers.py
+++ caseapi/serializers.py
@@ -1,6 +1,26 @@
 from rest_framework import serializers
 from .models import CaseMeta, USCircuitCaseMeta, Tag, Judges, USJudge
 
+
+# To allow choices to render with their labels, instead of their underlying representation
+# E.g. 'party': 'Democrat' instead of 'party': 0
+class ChoiceField(serializers.ChoiceField):
+    def to_representation(self, obj):
+        if obj == '' and self.allow_blank:
+            return obj
+        return self._choices[obj]
+
+    def to_internal_value(self, data):
+        # To support inserts with the value
+        if data == '' and self.allow_blank:
+            return ''
+
+        for key, val in self._choices.items():
+            if val == data:
+                return key
+        self.fail('invalid_choice', input=data)
+
+
 class CaseMetaSerializer(serializers.ModelSerializer):
     class Meta:
         model = CaseMeta
@@ -33,11 +53,15 @@ class TagSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class JudgeSerializer(serializers.ModelSerializer):
+    # For these fields, use the labels on the choice in models.py
+    judge_gender = ChoiceField(choices=Judges.GENDER_CHOICES)
     class Meta:
         model = Judges
         fields = '__all__'
 
 class USJudgeSerializer(serializers.ModelSerializer):
+    party = ChoiceField(choices=USJudge.PARTY_CHOICES)
+    judge_gender = ChoiceField(choices=Judges.GENDER_CHOICES)
     class Meta:
         model = USJudge
         fields = '__all__'
