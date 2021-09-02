a24514777c7e61bbe11c99ee4e843e53649e9e9b
Sanil Kumar Ashwin <sanil.kumar.ashwin@gmail.com>
2021-09-02 14:28:53 +0200

    Added all models to admin page

--- caseapi/admin.py
+++ caseapi/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import CaseMeta
+from .models import CaseMeta, USCircuitCaseMeta, Tag, JudgeRuling, Judges, USJudge
 
 # Register your models here.
 admin.site.register(CaseMeta)
+admin.site.register(USCircuitCaseMeta)
+admin.site.register(Judges)
+admin.site.register(USJudge)
+admin.site.register(JudgeRuling)
+admin.site.register(Tag)
