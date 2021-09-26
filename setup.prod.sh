57e802b7fb9d7c71d1bf705b3b6ba958b76b8310
Sanil Kumar Ashwin <sanil.kumar.ashwin@gmail.com>
2021-09-26 23:22:52 +0200

    Extended gunicorn timeout to 10 minutes

--- setup.prod.sh
+++ setup.prod.sh
@@ -3,4 +3,4 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py graph_models caseapi -g -o static/caseapi_model.png
-gunicorn caserepo.wsgi:application --bind 0.0.0.0:8000
+gunicorn caserepo.wsgi:application --bind 0.0.0.0:8000 --timeout=600
--- setup.test.sh
+++ setup.test.sh
@@ -4,4 +4,4 @@ sleep 5
 python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py graph_models caseapi -g -o static/caseapi_model.png
-gunicorn caserepo.wsgi:application --bind 0.0.0.0:8000
+gunicorn caserepo.wsgi:application --bind 0.0.0.0:8000 --timeout=600
