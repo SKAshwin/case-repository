214b8c18251efd7504532c8a2df63ad649d78b1d
Sanil Kumar Ashwin <sanil.kumar.ashwin@gmail.com>
2021-10-01 22:21:06 +0200

    Nevermind, need to allow errors

--- install_dump.sh
+++ install_dump.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker cp dump.sql db:dump.sql
-docker exec -it db psql -d caserepo -U caseapi_test -a -f dump.sql -v ON_ERROR_STOP=1
+docker exec -it db psql -d caserepo -U caseapi_test -a -f dump.sql
